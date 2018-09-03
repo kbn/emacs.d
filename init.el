3ace1ac49fabe0344d6c63514cfc58d365a412a7
Kristian Berge Nessa <kristian@cavatina.no>
2018-09-03 15:30:32 +0200

    Remove manual markdown-mode setup

--- init.el
+++ init.el
@@ -354,11 +354,6 @@ BEG and END (region to sort)."
 
 (load-init "htmlize")
 
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
 (autoload 'smart-tabs-mode "smart-tabs-mode"
   "Intelligently indent with tabs, align with spaces!")
  (autoload 'smart-tabs-mode-enable "smart-tabs-mode")
