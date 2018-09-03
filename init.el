0050a626427d26efc2d4f91995666e996661eb0f
Kristian Berge Nessa <kristian@cavatina.no>
2018-09-03 15:30:51 +0200

    Disable smart-tabs-mode

--- init.el
+++ init.el
@@ -354,13 +354,13 @@ BEG and END (region to sort)."
 
 (load-init "htmlize")
 
-(autoload 'smart-tabs-mode "smart-tabs-mode"
-  "Intelligently indent with tabs, align with spaces!")
- (autoload 'smart-tabs-mode-enable "smart-tabs-mode")
- (autoload 'smart-tabs-advice "smart-tabs-mode")
- (autoload 'smart-tabs-insinuate "smart-tabs-mode")
+;; (autoload 'smart-tabs-mode "smart-tabs-mode"
+;;   "Intelligently indent with tabs, align with spaces!")
+;;  (autoload 'smart-tabs-mode-enable "smart-tabs-mode")
+;;  (autoload 'smart-tabs-advice "smart-tabs-mode")
+;;  (autoload 'smart-tabs-insinuate "smart-tabs-mode")
 
-(smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
-		      'ruby 'nxml)
+;; (smart-tabs-insinuate 'c 'c++ 'java 'javascript 'cperl 'python
+;; 		      'ruby 'nxml)
 
 (require 'desktop)
