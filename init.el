cabbbb5212367df8e181546968542c32be33f762
Kristian Berge Nessa <kristian@cavatina.no>
2018-09-03 15:10:24 +0200

    Use Consolas font if DejaVu is unavailable

--- init.el
+++ init.el
@@ -8,6 +8,10 @@
     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
   (package-initialize))
 
+(defvar default-font-family (if (member "Consolas" (font-family-list))
+				"Consolas"
+			      "DejaVu Sans Mono"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -72,7 +76,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:slant normal :weight normal :height 101 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(default ((t (:slant normal :weight normal :height 101 :width normal :foundry "unknown" :family font-family-default))))
  '(diff-added ((t (:foreground "forest green"))))
  '(diff-removed ((t (:foreground "red3"))))
  '(diff-removed-face ((t (:foreground "Red"))) t)
